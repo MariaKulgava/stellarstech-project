8b6822168fab622c88c5c7eb97fb73674a8e9507
Maria Kulgava <mashak@propertyvista.com>
2024-12-11 20:37:16 +0200

    Added ID verification to tests

--- src/test/java/PetstoreAPITest.java
+++ src/test/java/PetstoreAPITest.java
@@ -15,23 +15,25 @@ public class PetstoreAPITest {
     public static Playwright playwright;
     public static APIRequestContext apiRequestContext;
     private static String randomUsername;
+    private static int randomId;
 
     @BeforeClass
     public void setup() {
         playwright = Playwright.create();
         apiRequestContext = playwright.request().newContext(
-                new APIRequest.NewContextOptions().setBaseURL("https://petstore.swagger.io")
+                new APIRequest.NewContextOptions().setBaseURL("https://petstore.swagger.io/v2/")
         );
 
         // Generate random username
         randomUsername = DataGenerator.generateRandomUsername();
+        randomId = DataGenerator.generateRandomId();
     }
 
     @Test(priority = 1)
     public void testCreateUser() {
         String requestBody = """
                 {
-                    "id": 0,
+                    "id": %d,
                     "username": "%s",
                     "firstName": "Test",
                     "lastName": "User",
@@ -40,31 +42,35 @@ public class PetstoreAPITest {
                     "phone": "1234567890",
                     "userStatus": 1
                 }
-                """.formatted(randomUsername);
+                """.formatted(randomId, randomUsername);
 
         APIResponse response = apiRequestContext.post(
-                "/v2/user",
+                "user",
                 RequestOptions.create()
                         .setHeader("Content-Type", "application/json")
                         .setData(requestBody)
         );
 
         assertEquals(response.status(), 200, "The response status should be 200");
-        System.out.println("User created with username: " + randomUsername);
+        assertTrue(response.text().contains("\"message\":\"" + randomId + "\""),
+                "Response should contain 'message: " + randomId + "'");
+        System.out.println("User created with username: " + randomUsername + " and ID: " + randomId);
     }
 
     @Test(priority = 2, dependsOnMethods = "testCreateUser")
     public void testGetUser() {
-        APIResponse response = apiRequestContext.get("/v2/user/" + randomUsername);
+        APIResponse response = apiRequestContext.get("user/" + randomUsername);
 
         assertEquals(response.status(), 200, "The response status should be 200");
         assertTrue(response.text().contains(randomUsername), "The response should contain the username");
-        System.out.println("User exists: " + randomUsername);
+        assertTrue(response.text().contains("\"id\":" + randomId),
+                "Response should contain 'id: " + randomId + "'");
+        System.out.println("User exists: " + randomUsername + " and ID: " + randomId);
     }
 
     @Test(priority = 3, dependsOnMethods = "testGetUser")
     public void testDeleteUser() {
-        APIResponse response = apiRequestContext.delete("/v2/user/" + randomUsername);
+        APIResponse response = apiRequestContext.delete("user/" + randomUsername);
 
         assertEquals(response.status(), 200, "The response status should be 200");
         System.out.println("User deleted: " + randomUsername);
@@ -72,7 +78,7 @@ public class PetstoreAPITest {
 
     @Test(priority = 4, dependsOnMethods = "testDeleteUser")
     public void testGetUserNotFound() {
-        APIResponse response = apiRequestContext.get("/user/" + randomUsername);
+        APIResponse response = apiRequestContext.get("user/" + randomUsername);
 
         assertEquals(response.status(), 404, "The response status should be 404");
         System.out.println("User not found: " + randomUsername);
--- src/test/java/utils/DataGenerator.java
+++ src/test/java/utils/DataGenerator.java
@@ -6,4 +6,8 @@ public class DataGenerator {
     public static String generateRandomUsername() {
         return "User_" + UUID.randomUUID().toString().substring(0, 8);
     }
+
+    public static int generateRandomId() {
+        return (int) (Math.random() * 1_000_000);
+    }
 }
